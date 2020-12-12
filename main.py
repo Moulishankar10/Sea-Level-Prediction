01fdab5d357dafe3b434ef7f5d1d82cafa0768cf
Moulishankar10 <moulishankarmr@gmail.com>
2020-12-12 20:19:47 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# SEA LEVEL PREDICTION
 
+# DEVELOPED BY: 
+# MOULISHANKAR M R
+# VIGNESHWAR RAVICHANDAR
+
+# IMPORTING MODULES
+import os
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
