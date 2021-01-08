cc6acf7e2a6030b17e95ed25c8450b2f8b827f3f
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-08 19:26:52 +0530

    Preprocessed data

--- test.py
+++ test.py
@@ -13,3 +13,16 @@ from sklearn.preprocessing import MinMaxScaler
 
 # IMPORTING DATA
 data = pd.read_csv("data/data.csv")
+
+# PREPROCESSING DATA
+x = []
+initial_str = data["Time"][0]
+initial = dt.datetime(int(initial_str[-4:]),int(initial_str[:2]),1)
+ 
+for i in range(len(data["Time"])):
+    final_str = data["Time"][i]
+    final = dt.datetime(int(final_str[-4:]),int(final_str[:2]),1)
+    diff = (final.year - initial.year) * 12 + (final.month - initial.month)
+    x.append(diff)
+
+y = data["GMSL"].values
