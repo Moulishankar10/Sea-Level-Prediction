5e5daccf8be2cbacfe7a231b1fc83f0b7283f9e4
Moulishankar10 <moulishankarmr@gmail.com>
2021-01-08 13:26:42 +0530

    Added code for saving model

--- main.py
+++ main.py
@@ -76,3 +76,8 @@ ypred_scaled = model.predict(xval_scaled)
 y_pred = scaler_y.inverse_transform(ypred_scaled)
 acc_score = r2_score(y_val, y_pred)
 print(f"\nAccuracy of the model : {round(acc_score*100,2)}%")
+
+# SAVING THE TRAINED MODEL
+PATH = './model/model'
+save_model(model,PATH)
+print(f"\n\n ---- Successfully stored the trained model at {PATH} ---- \n\n")
