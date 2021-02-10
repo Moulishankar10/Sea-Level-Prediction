41d9f00d02e4f4907ad93677ac67038674e08796
Moulishankar10 <moulishankarmr@gmail.com>
2021-02-10 18:47:26 +0530

    Updated the code

--- test.py
+++ test.py
@@ -60,4 +60,4 @@ ypred_scaled = model.predict(xpred_scaled)
 y_pred = scaler_y.inverse_transform(ypred_scaled)
 
 # DISPLAYING THE RESULTS
-print(f"\n\n As per the prediction, on {month[int(input_month[:2])-1]} {int(input_month[-4:])}, the value of Sea Level might be -> Rs. {round(float(y_pred),4)} (Monthly Average)\n\n")
+print(f"\n\n As per the prediction, by {month[int(input_month[:2])-1]} {int(input_month[-4:])}, the Sea Level might be changed to {round(float(y_pred),4)} meters (Monthly Average)\n\n")
